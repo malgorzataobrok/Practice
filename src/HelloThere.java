d56fb6ce62b27c55dee10f28dafa84ba5c12a512
Malgorzata_Obrok <malgorzata.obrok@motorolasolutions.com>
2022-09-28 11:40:45 +0200

    nationality "polish" added

--- src/HelloThere.java
+++ src/HelloThere.java
@@ -1,5 +1,8 @@
 public class HelloThere {
+
+    private String nationality = "polish";
         static void helloPeople(String nationality){
+
             System.out.println("Hi " + nationality + "people!");
         }
     }
